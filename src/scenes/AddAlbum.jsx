bf6d467e23aae08e10be1ffd01c441ecddc4ec62
Midokhalil1 <midokhalil22@gmail.com>
2022-11-02 10:23:10 -0400

    album2

---
+++ src/scenes/AddAlbum.jsx
@@ -0,0 +1,9 @@
+export default function AddAlbum() {
+    return (
+        <>
+            <h1>Add Album Title goes here</h1>
+            <p> New Album card goes here </p>
+
+        </>
+    )
+}
---
+++ src/scenes/SingleAlbum.jsx
@@ -0,0 +1,9 @@
+export default function SingleAlbum() {
+    return (
+        <>
+            <h1> Album Title goes here...</h1>
+            <p> Album card goes here </p>
+
+        </>
+    )
+}
